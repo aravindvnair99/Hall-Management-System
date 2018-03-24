9d62260bdbda56011a015ad926849afcc22d402f
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-03-24 18:51:33 +0530

    DB connect

--- test.js
+++ test.js
@@ -2,12 +2,19 @@ var express = require("express"),
     app = express(),
     path = __dirname + '/views/',
     bodyParser = require('body-parser'),
-    router = express.Router();
+    router = express.Router(),
+    mysql = require('mysql');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-mysql = require('mysql');
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', path);
+app.set('view engine', 'ejs');
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
 var con = mysql.createConnection({
     host: "localhost",
     user: 'root',
@@ -23,13 +30,6 @@ con.connect(function (error) {
     } else {
         console.log(" Connected Yo!");
     }
-});
-app.set('port', (process.env.PORT || 8000));
-app.use(express.static(__dirname + '/public'));
-app.set('views', path);
-app.set('view engine', 'ejs');
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
 });
 app.get('/', function (req, res) {
     res.render('index');
@@ -95,7 +95,7 @@ app.get('/request', function (req, res) {
 app.get('/logout', function (req, res) {
     res.render('logout');
 });
-app.get('/onLogin', function (req, res) {
+app.post('/onLogin', function (req, res) {
     console.log("Before Login");
     var username = req.body.username;
     var password = req.body.password;
@@ -103,25 +103,23 @@ app.get('/onLogin', function (req, res) {
     con.query("select * from users where username='" + username + "' and role='1';", function (err, rows) {
         if (!err) {
             if (rows.length > 0) {
-
-                if (password === rows[0].Password) {
+                console.log(rows.length);
+                console.log(rows);
+                if (password === rows[0].password) {
                     res.render("dashboard");
                 } else {
                     console.log("Password is Wrong Buddy!");
                     res.render("error", { message: "Password is Wrong Buddy!" });
                 }
-
             } else {
                 console.log("Username Not Found!");
                 res.render("error", { message: "Username Not Found!" });
             }
-
-
         } else {
-
             res.render("error", { message: "Dont Poke Your Nose where you don't Belong!" });
         }
-        console.log("After Login");    
+    });
+    console.log("After Login");    
 });
 app.get('/getUserProfile', function (req, res) {
     console.log("getUserProfile");
@@ -147,7 +145,7 @@ app.get('/submitRequest', function (req, res) {
     console.log("submitRequest");
     res.send('Need to add. Contact Aravind.');
 });
-app.get('*', function(req, res){
+app.get('*', function(req, res) {
     console.log("404");
     res.status(404).send('Error 404 - Not Found Contact Aravind.');
 });
