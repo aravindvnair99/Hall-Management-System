cfbb9f8b6189b50817464ff7c20053bbd646367e
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-11-10 18:05:09 +0530

    Update SQl commands
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,3 +1,10 @@
 -- To retrieve all sequences:
 
 select * from information_schema.columns where extra like '%auto_increment%';
+
+-- To check if hall is available on a particular day
+
+select hall_id from hall_schedule where booking_id in (select booking_id from slot_schedule where booking_id in (select id from booking where event_id in (select id from events WHERE date_wanted='2019-11-11')) and slot_id='10')
+
+-- To retrieve available slots for a particular hall
+
