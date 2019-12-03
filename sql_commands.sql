b4ce83e14c29ac7f7e957ecdb370217cffe54b27
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-12-03 22:50:33 +0530

    Cleanup
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- sql_commands.sql
+++ sql_commands.sql
@@ -1,7 +1,3 @@
 -- To retrieve all sequences:
 
 select * from information_schema.columns where extra like '%auto_increment%';
-
--- To check if hall is available on a particular day and to retrieve available slots for a particular hall
-
-SELECT halls FROM booking WHERE event_id IN (SELECT id FROM events WHERE date_wanted = '2019-11-14')
